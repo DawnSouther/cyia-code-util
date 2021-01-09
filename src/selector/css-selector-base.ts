4fdca3a21a95a53a58d12eb5e89bb4ac924ff55c
wszgrcy <wszgrcy@gmail.com>
2021-01-09 09:14:23 +0800

    feat(选择器): 增加基类

---
+++ src/selector/css-selector-base.ts
@@ -0,0 +1,156 @@
+import { AttributeSelector, parse, Selector } from 'css-what';
+export class NodeContext<T> {
+    constructor(public node: T, public parent: NodeContext<T>, public index: number) {}
+}
+export abstract class CssSelectorBase<NODE, PARSETREE> {
+    protected abstract parseTree: PARSETREE;
+    private currentNodeList: NodeContext<NODE>[];
+    private multi = true;
+    protected abstract getTagAttribute(selector: AttributeSelector, node: NODE): { value: string };
+    protected abstract findTag(name: string, node: NODE): boolean;
+    protected findAttribute(selector: AttributeSelector, node: NODE): boolean {
+        let attribute = this.getTagAttribute(selector, node);
+        if (attribute) {
+            let str: string;
+            try {
+                str = attribute.value;
+                switch (selector.action) {
+                    case 'equals':
+                        return str === selector.value;
+                    case 'any':
+                        return str.includes(selector.value);
+                    case 'start':
+                        return str.startsWith(selector.value);
+                    case 'end':
+                        return str.endsWith(selector.value);
+                    case 'element':
+                        return str.split(' ').includes(selector.value);
+                    case 'hyphen':
+                        return str.split(' ')[0].split('-')[0] === selector.value;
+                    case 'not':
+                        return str !== selector.value;
+                    case 'exists':
+                        return !!attribute;
+                    default:
+                        return str === selector.value;
+                }
+            } catch (error) {}
+        }
+        return false;
+    }
+    protected findWithEachNode(context: NodeContext<NODE>, fn: (node: NODE) => boolean, multi: boolean): NodeContext<NODE>[] {
+        let list: NodeContext<NODE>[] = [context];
+        let result = [];
+        while (list.length) {
+            let node = list.pop();
+            if (fn(node.node)) {
+                result.push(node);
+            }
+            if (multi) {
+                list.push(...this.getChildren(node.node).map((childNode, i) => new NodeContext(childNode, node, i)));
+            }
+        }
+        return result;
+    }
+    protected abstract getChildren(node: NODE): NODE[];
+    protected abstract getQueryNode(node?: NODE): NODE;
+    query(node: NODE, selector: string): NODE[];
+    query(selector: string): NODE[];
+    query(arg1: any, arg2?) {
+        let selector: string;
+        let queryElement: NODE;
+
+        if (typeof arg1 === 'string') {
+            selector = arg1;
+            queryElement = this.getQueryNode(undefined);
+        } else {
+            selector = arg2;
+            queryElement = this.getQueryNode(arg1);
+        }
+        let selectedList: NODE[] = [];
+
+        let result = parse(selector, { lowerCaseAttributeNames: false, lowerCaseTags: false });
+        /** 使用一个虚拟节点支持搜索 */
+        for (let i = 0; i < result.length; i++) {
+            this.currentNodeList = [new NodeContext(queryElement, undefined, undefined)];
+            const selectorList = result[i];
+            for (let j = 0; j < selectorList.length; j++) {
+                const selector = selectorList[j];
+                if (!this.parse(selector)) {
+                    break;
+                }
+            }
+            selectedList.push(...this.currentNodeList.map((node) => node.node));
+        }
+        return selectedList;
+    }
+    protected parse(selector: Selector): boolean {
+        let list: NodeContext<NODE>[] = [];
+        switch (selector.type) {
+            // 匹配标签
+            case 'tag':
+                this.currentNodeList.forEach((nodeContext: NodeContext<NODE>) => {
+                    list = list.concat(this.findWithEachNode(nodeContext, (node) => this.findTag(selector.name, node), this.multi));
+                });
+                this.currentNodeList = list;
+                this.multi = true;
+                break;
+            // 空格
+            case 'descendant':
+                this.currentNodeList = [].concat(
+                    ...this.currentNodeList.map((node) => this.getChildren(node.node).map((child, i) => new NodeContext(child, node, i)))
+                );
+                break;
+            //+
+            case 'adjacent':
+                this.currentNodeList = [].concat(
+                    this.currentNodeList
+                        .map(
+                            (nodeContext) =>
+                                new NodeContext(
+                                    this.getChildren(nodeContext.parent.node)[nodeContext.index + 1],
+                                    nodeContext.parent,
+                                    nodeContext.index + 1
+                                )
+                        )
+                        .filter((node) => node.node)
+                );
+
+                this.multi = false;
+                break;
+            // >
+            case 'child':
+                this.currentNodeList = [].concat(
+                    ...this.currentNodeList.map((node) => this.getChildren(node.node).map((child, i) => new NodeContext(child, node, i)))
+                );
+                this.multi = false;
+                break;
+            // ~
+            case 'sibling':
+                this.currentNodeList = [].concat(
+                    ...this.currentNodeList.map((nodeContent) =>
+                        this.getChildren(nodeContent.parent.node)
+                            .filter((node, i) => i > nodeContent.index)
+                            .map((node, i) => new NodeContext(node, nodeContent.parent, i))
+                    )
+                );
+
+                this.multi = false;
+                break;
+            //[] .xxx
+            case 'attribute':
+                this.currentNodeList.forEach((nodeContext: NodeContext<NODE>) => {
+                    list = list.concat(
+                        this.findWithEachNode(nodeContext, (node) => this.findAttribute(selector as AttributeSelector, node), this.multi)
+                    );
+                });
+                this.currentNodeList = list;
+                this.multi = true;
+                break;
+            default:
+                break;
+        }
+
+        return !!this.currentNodeList.length;
+    }
+}
