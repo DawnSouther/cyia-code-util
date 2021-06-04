929b83c97ac45950d809bb00fdd507fa77ba6541
chen-yang <wszgrcy@gmail.com>
2021-06-04 10:19:41 +0800

    chore: 导出变更

--- src/change/index.ts
+++ src/change/index.ts
@@ -1,1 +1,3 @@
 export * from './ts-change';
+export * from './html-change';
+export * from './json-change';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './interactive';
 export * from './selector';
+export * from './change';
