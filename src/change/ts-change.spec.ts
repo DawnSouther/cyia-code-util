f65c0254c68be01ff275059a6c2accefb3b91583
wszgrcy <wszgrcy@gmail.com>
2021-07-20 15:20:30 +0800

    feat: 增加tschange操作

--- src/change/ts-change.spec.ts
+++ src/change/ts-change.spec.ts
@@ -62,4 +62,119 @@ describe('ts-change', () => {
         let newContent = UpdaterTest.update(content, result);
         expect(newContent).toBe(`let a=abc/**number*/2`);
     });
+    it('插入子节点内容(对象)', () => {
+        let file = ts.createSourceFile('', `let a={b:1}`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ObjectLiteralExpression') as ts.ObjectLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, 'abc:2');
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a={b:1,abc:2}`);
+    });
+    it('插入子节点内容(对象,尾逗号)', () => {
+        let file = ts.createSourceFile('', `let a={b:1/**注释*/,}`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ObjectLiteralExpression') as ts.ObjectLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, 'abc:2');
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a={b:1/**注释*/,abc:2}`);
+    });
+    it('插入子节点内容(对象,尾逗号,位置)', () => {
+        let file = ts.createSourceFile('', `let a={b:1/**注释*/,}`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ObjectLiteralExpression') as ts.ObjectLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, 'abc:2', 0);
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a={abc:2,b:1/**注释*/,}`);
+    });
+    it('插入子节点内容(空对象)', () => {
+        let file = ts.createSourceFile('', `let a={}`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ObjectLiteralExpression') as ts.ObjectLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, 'abc:2');
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a={abc:2}`);
+    });
+    it('插入子节点内容(空数组)', () => {
+        let file = ts.createSourceFile('', `let a=[]`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ArrayLiteralExpression') as ts.ArrayLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, '1');
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a=[1]`);
+    });
+    it('插入子节点内容(数组)', () => {
+        let file = ts.createSourceFile('', `let a=[2]`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ArrayLiteralExpression') as ts.ArrayLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, '1');
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a=[2,1]`);
+    });
+    it('插入子节点内容(数组,位置)', () => {
+        let file = ts.createSourceFile('', `let a=[2]`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ArrayLiteralExpression') as ts.ArrayLiteralExpression;
+        let change = new TsChange(file);
+        let result = change.insertChildNode(node, '1', 0);
+        let newContent = UpdaterTest.update(file.text, result);
+        expect(newContent).toBe(`let a=[1,2]`);
+    });
+    it('删除子节点内容(对象)', () => {
+        let file = ts.createSourceFile('', `let a={a:1/**abc*/,b:2,c:3}`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ObjectLiteralExpression') as ts.ObjectLiteralExpression;
+        let change = new TsChange(file);
+        let result = change
+            .deleteChildNode(node, (node) => {
+                return node.getText().includes('a');
+            })
+            .sort((a, b) => b.start - a.start);
+        let newContent = file.text;
+        result.forEach((change) => {
+            newContent = UpdaterTest.update(newContent, change);
+        });
+        expect(newContent).toBe(`let a={b:2,c:3}`);
+        result = change
+            .deleteChildNode(node, (node) => {
+                return node.getText().includes('b');
+            })
+            .sort((a, b) => b.start - a.start);
+        newContent = file.text;
+        result.forEach((change) => {
+            newContent = UpdaterTest.update(newContent, change);
+        });
+        expect(newContent).toBe(`let a={a:1/**abc*/,c:3}`);
+    });
+    it('删除子节点内容(数组)', () => {
+        let file = ts.createSourceFile('', `let a=[1/**abc*/,2,3]`, ts.ScriptTarget.Latest, true);
+        let selector = createCssSelectorForTs(file);
+        let node = selector.queryOne('ArrayLiteralExpression') as ts.ArrayLiteralExpression;
+        let change = new TsChange(file);
+        let result = change
+            .deleteChildNode(node, (node) => {
+                return node.getText().includes('1');
+            })
+            .sort((a, b) => b.start - a.start);
+        let newContent = file.text;
+        result.forEach((change) => {
+            newContent = UpdaterTest.update(newContent, change);
+        });
+        expect(newContent).toBe(`let a=[2,3]`);
+        result = change
+            .deleteChildNode(node, (node) => {
+                return node.getText().includes('2');
+            })
+            .sort((a, b) => b.start - a.start);
+        newContent = file.text;
+        result.forEach((change) => {
+            newContent = UpdaterTest.update(newContent, change);
+        });
+        expect(newContent).toBe(`let a=[1/**abc*/,3]`);
+    });
 });
--- src/change/ts-change.ts
+++ src/change/ts-change.ts
@@ -1,4 +1,4 @@
-import { Node, SourceFile } from 'typescript';
+import ts, { isArrayLiteralExpression, isObjectLiteralExpression, Node, PropertyAssignment, SourceFile } from 'typescript';
 import { DeleteChange, InsertChange, ReplaceChange } from './content-change';
 
 interface NodeOptions {
@@ -21,7 +21,53 @@ export class TsChange {
 
         return new InsertChange(pos === 'start' ? start : start + this.getNodeWidth(node, options), content, pos);
     }
-
+    insertChildNode(node: ts.ObjectLiteralExpression | ts.ArrayLiteralExpression, content: string, index?: number) {
+        let start: number;
+        let list: ts.NodeArray<Node>;
+        let startComma: boolean = false;
+        let endComma: boolean = false;
+        if (isObjectLiteralExpression(node)) {
+            list = node.properties;
+        } else if (isArrayLiteralExpression(node)) {
+            list = node.elements;
+        }
+        index = typeof index === 'undefined' ? list.length : index;
+        if (index === list.length && index !== 0 && !list.hasTrailingComma) {
+            startComma = true;
+        }
+        if (index !== list.length) {
+            endComma = true;
+        }
+        if (index === 0) {
+            start = list.pos;
+        } else if (index > list.length || index === list.length) {
+            start = list.end;
+        } else {
+            start = list[index - 1].pos + this.getNodeWidth(list[index - 1], { full: true });
+        }
+        return new InsertChange(start, `${startComma ? ',' : ''}${content}${endComma ? ',' : ''}`);
+    }
+    deleteChildNode(
+        node: ts.ObjectLiteralExpression | ts.ArrayLiteralExpression,
+        deleteFunction: (node: ts.Node, index: number) => boolean
+    ) {
+        let list: ts.NodeArray<Node>;
+        if (isObjectLiteralExpression(node)) {
+            list = node.properties;
+        } else if (isArrayLiteralExpression(node)) {
+            list = node.elements;
+        }
+        let deleteList: DeleteChange[] = [];
+        list.forEach((node, index) => {
+            if (!deleteFunction(node, index)) {
+                return;
+            }
+            let start = index === 0 ? list.pos : node.pos;
+            let end = index === list.length - 1 ? list.end : list[index + 1].pos;
+            deleteList.push(new DeleteChange(start, end - start));
+        });
+        return deleteList;
+    }
     private getNodeStart(node: Node, options: NodeOptions = {}) {
         return options.full ? node.getFullStart() : node.getStart(this.sourceFile, options.comment);
     }
